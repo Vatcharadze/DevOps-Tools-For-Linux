15244fbe16b46493167bdcbf7a60c69557bc8afe
dato <dato.vatcharadze@gmail.com>
2026-01-01 20:33:11 +0400

    Edited DevOpsEssentials.sh File

--- DevOpsEssentials.sh
+++ DevOpsEssentials.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-echo "===Hello $USER==="
-echo "===Installing Essential Tools For DevOps==="
+set -e
+
+echo -e "\e[1;45m===HELLO $USER !===\e[0m"
+echo -e "\e[1;45m===Installing Essential Tools For Devops===\e[0m"
 
 sudo apt update -y
-
-echo "===Installing Git==="
+echo -e "\e[5;41m===Installing Git===\e[0m"
 sudo apt install git -y
 
-echo "===Installing Vim Editor==="
+echo -e "\e[5;41m===Installing Vim Editor===\e[0m"
 sudo apt install vim -y 
 
-echo "===Installing Docker==="
+echo -e "\e[5;41m===Installing Docker===\e[0m"
 sudo apt install docker.io -y
 
-echo "===Installing Docker Compose==="
+#Adding USER To docker Group So You Can Use docker Command Without sudo 
+sudo usermod -aG docker $USER
+
+echo -e "\e[5;41m===Installing Docker Compose===\e[0m"
 sudo apt install docker-compose -y
 
-echo "===Installing Kubernetes==="
+echo -e "\e[5;41m===Installing Kubernetes===\e[0m"
 sudo snap install kubectl --classic
 
-echo "===Installing Terraform==="
+echo -e "\e[5;41m===Installing Terraform===\e[0m"
 sudo snap install terraform --classic
 
-echo "===Installing Ansible==="
+echo -e "\e[5;41m===Installing Ansible===\e[0m"
 sudo apt install ansible -y
 
-echo "===Installing Htop/Net-Tools==="
+echo -e "\e[5;41m===Installing Htop/Net-Tools===\e[0m"
 sudo apt install htop -y net-tools -y
 
-echo "***This Is A Typical Stack For DevOps***"
-echo "*** Git + Docker + Kubernetes + Terraform + Ansible + CI/CD + Monitoring ***"
+echo -e "\e[1;45m***This Is A Typical Stack For DevOps***\e[0m"
+echo -e "\e[1;45m*** Git + Docker + Kubernetes + Terraform + Ansible + CI/CD + Monitoring ***\e[0m"
 
 
